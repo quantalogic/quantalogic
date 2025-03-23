199bde371522c354e746f85c68ea00eeea43e09a
Raphaël MANSUY <raphael.mansuy@gmail.com>
2025-03-23 09:49:17 +0800

    move prompt

--- quantalogic/codeact/constants.py
+++ quantalogic/codeact/constants.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-BASE_DIR = Path(__file__).parent.parent
+BASE_DIR = Path(__file__).parent
 TEMPLATE_DIR = BASE_DIR / "prompts"
 LOG_FILE = "react_agent.log"
 DEFAULT_MODEL = "gemini/gemini-2.0-flash"
