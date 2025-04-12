a2b430301b4ad1998f3106cedab24584cda97bc5
Raphaël MANSUY <raphael.mansuy@gmail.com>
2025-04-12 16:18:52 +0800

    Refactor imports and simplify tool function references in planning toolbox

--- toolboxes/quantalogic_planning_toolbox/quantalogic_planning_toolbox/__init__.py
+++ toolboxes/quantalogic_planning_toolbox/quantalogic_planning_toolbox/__init__.py
@@ -1,4 +1,10 @@
-import quantalogic_planning_toolbox.tools
+from .tools import (
+    create_project_plan,
+    get_subtasks_by_status,
+    retrieve_project_plan,
+    update_subtask_status_by_id,
+    update_subtasks_status,
+)
 
 __all__ = [
     "create_project_plan",
@@ -12,10 +18,10 @@ __all__ = [
 def get_tools():
     """Return a list of tool functions for registration."""
     return [
-        quantalogic_planning_toolbox.tools.create_project_plan,
-        quantalogic_planning_toolbox.tools.get_subtasks_by_status,
-        quantalogic_planning_toolbox.tools.retrieve_project_plan,
-        quantalogic_planning_toolbox.tools.update_subtask_status_by_id,
-        quantalogic_planning_toolbox.tools.update_subtasks_status
+        create_project_plan,
+        get_subtasks_by_status,
+        retrieve_project_plan,
+        update_subtask_status_by_id,
+        update_subtasks_status
     ]
     
